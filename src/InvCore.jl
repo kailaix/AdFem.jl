a618b58db889b64e5da9a7b4d151c3dd301fdf00
Kailai Xu <kailaix@hotmail.com>
2020-02-09 20:30:16 -0800

    update

--- src/InvCore.jl
+++ src/InvCore.jl
@@ -6,6 +6,20 @@ function fem_impose_coupled_Dirichlet_boundary_condition(A::SparseTensor, bd::Ar
     SparseTensor(ii1,jj1,vv1,2(m+1)*(n+1)+m*n,2(m+1)*(n+1)+m*n), SparseTensor(ii2,jj2,vv2,2(m+1)*(n+1)+m*n,2length(bd))
 end
 
+function fem_impose_Dirichlet_boundary_condition(L, bdnode, m, n, h)
+    idx = [bdnode; bdnode .+ (m+1)*(n+1)]
+    Lbd = L[:, idx]
+    L = scatter_update(L, :, idx, spzero(2*(m+1)*(n+1), length(idx)))
+    L = scatter_update(L, idx, :,  spzero(length(idx), 2*(m+1)*(n+1)))
+    L = scatter_update(L, idx, idx, spdiag(length(idx)))
+    L, Lbd
+end
+
+"""
+    compute_fem_stiffness_matrix(hmat::PyObject, m::Int64, n::Int64, h::Float64)
+
+A differentiable kernel
+"""
 function compute_fem_stiffness_matrix(hmat::PyObject, m::Int64, n::Int64, h::Float64)
     fem_stiffness_ = load_op_and_grad("$(@__DIR__)/../deps/FemStiffness/build/libFemStiffness","fem_stiffness", multiple=true)
     hmat,m_,n_,h = convert_to_tensor([hmat,m,n,h], [Float64,Int32,Int32,Float64])
@@ -21,9 +35,9 @@ A differentiable kernel.
 """
 function compute_strain_energy_term(S::PyObject,m::Int64, n::Int64, h::Float64)
     strain_energy_ = load_op_and_grad("$(@__DIR__)/../deps/StrainEnergy/build/libStrainEnergy","strain_energy")
-    sigma,m,n,h = convert_to_tensor([S,m,n,h], [Float64,Int32,Int32,Float64])
-    out = strain_energy_(sigma,m,n,h)
-    out.set_shape(2*(m+1)*(n+1))
+    sigma,m_,n_,h = convert_to_tensor([S,m,n,h], [Float64,Int32,Int32,Float64])
+    out = strain_energy_(sigma,m_,n_,h)
+    out.set_shape((2*(m+1)*(n+1),))
     out
 end
 
@@ -34,8 +48,8 @@ A differentiable kernel.
 """
 function eval_strain_on_gauss_pts(u::PyObject, m::Int64, n::Int64, h::Float64)
     strain_op_ = load_op_and_grad("$(@__DIR__)/../deps/Strain/build/libStrainOp","strain_op")
-    u,m,n,h = convert_to_tensor([u,m,n,h], [Float64,Int32,Int32,Float64])
-    out = strain_op_(u,m,n,h)
+    u,m_,n_,h = convert_to_tensor([u,m,n,h], [Float64,Int32,Int32,Float64])
+    out = strain_op_(u,m_,n_,h)
     out.set_shape((4*m*n, 3))
     out 
 end
