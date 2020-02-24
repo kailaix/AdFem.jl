cfe1ac38197a3d4f3ebf02205868a27169a52fe0
Kailai Xu <kailaix@hotmail.com>
2020-02-24 15:44:15 -0800

    add precompile

--- src/PoreFlow.jl
+++ src/PoreFlow.jl
@@ -9,10 +9,15 @@ module PoreFlow
     using Reexport
     using Statistics
     @reexport using ADCME
-    np = pyimport("numpy")
-    interpolate = pyimport("scipy.interpolate")
 
     pts = @. ([-1/sqrt(3); 1/sqrt(3)] + 1)/2
+    np = PyNULL()
+    interpolate = PyNULL()
+    function __init__()
+        global np, interpolate
+        np = pyimport("numpy")
+        interpolate = pyimport("scipy.interpolate")
+    end
 
     include("Struct.jl")
     include("Utils.jl")
