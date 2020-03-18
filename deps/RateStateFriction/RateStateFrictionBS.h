0d00dbd6654adc819afd760c79a92c1db33d245d
Kailai Xu <kailaix@stanford.edu>
2020-03-18 16:45:13 -0700

    update
    Co-authored-by: weiqiang <wayne.weiqiang@gmail.com>

--- deps/RateStateFriction/RateStateFrictionBS.h
+++ deps/RateStateFriction/RateStateFrictionBS.h
@@ -1,5 +1,5 @@
 #include <math.h>       /* asinh, exp, cosh */
-
+#include <assert.h>
 
 // o/sqrt(a^2+o^2) = 1/sqrt(a^2 o^{-2} + 1)
 
@@ -14,72 +14,92 @@ double f(double u, double psi, double a, double uold,
   double inv_o = exp(-psi/a);
   double log_o = psi/a;
   double s = (u-uold) / deltat / 2.0 / v0;
-//   printf("%e %e\n", s, asinh_exp(s, inv_o, log_o));
   double F = a * asinh_exp(s, inv_o, log_o) * sigma - tau + eta * (u-uold) / deltat;
   return F; 
 }
 
 double Bisection(double xR, double xL, double psi, 
         double a, double uold, double deltat, double eta, double v0, double tau, double sigma){
+
+    assert(psi>0);
+    assert(a>0);
+    assert(eta>0);
+    assert(v0>0);
+    assert(tau>0);
+    assert(sigma>0);
+    assert(uold>0);
+
     double fL = f(xL, psi, a, uold, deltat, eta, v0, tau, sigma);
     double fR = f(xR, psi, a, uold, deltat, eta, v0, tau, sigma);
-    int maxIter = 500;
+    int maxIter = 1000000;
     double xTol = 1e-10;
     double fTol = 1e-10;
     if (fL*fR>0){
-        printf("Initial guess not valid\n");
-        exit(1);
+        printf("Initial guess (%e, %e) (resulting f = (%e, %e)) not valid\n", xL, xR, fL, fR);
+        // exit(1);
         return -1; 
     }
 
-    double xM = (xL+xR)/2;
-    double fM = f(xM, psi, a, uold, deltat, eta, v0, tau, sigma);
+    // double xM = (xL+xR)/2;
+    // double fM = f(xM, psi, a, uold, deltat, eta, v0, tau, sigma);
+    double fM = fL;
+    double xM = xL;
     double xErr = abs(xM - xL);
     double fErr = abs(fM);
     int numIter = 1;
 
+    // assumption: fL < 0, fR > 0
+
+    // assert(fL<0);
+    // assert(fR>0);
+
     // printf("iter = 1, fL=%e, fM=%e, fR=%e\n", fL, fM, fR);
     while (true){
-        // printf("iter = %d, x = (%e, %e), f = (%e, %e)\n", numIter, xL, xR, fL, fR);
+        
         if (numIter>maxIter || (xErr<xTol && fErr <= fTol) )
             break;
-        if (fL*fM<=0){
+        xM = (xL+xR)/2;
+        fM = f(xM, psi, a, uold, deltat, eta, v0, tau, sigma);
+
+        // printf("iter = %d, x = (%e, %e, %e), f = (%e, %e, %e)\n", numIter, xL-2, xM-2, xR-2, fL, fM, fR);
+        if (fL*fM<0){
             xR = xM;
             fR = fM;
         }      
-        else {
+        else if (fL*fM>0) {
             xL = xM; 
             fL = fM;
         }
+        else{
+            break;
+        }
         
-        xM = (xL+xR)/2;
-        fM = f(xM, psi, a, uold, deltat, eta, v0, tau, sigma);
-        xErr = abs(xM-xL);
+        xErr = abs(xR-xL);
         fErr = abs(fM);
         numIter += 1;
     }
-    if (numIter>maxIter){
-        printf("WARNING: Bisection Does Find Optimal Solution, use Newton's method for refinement\n");
-        int iter;
-        for (iter=0; iter<100; iter++) {
-            double inv_o = exp(-psi/a);
-            double log_o = psi/a;
-            double s = (xM-uold) / deltat / 2.0 / v0;
+    // if (numIter>maxIter){
+    //     printf("WARNING: Bisection Does Find Optimal Solution, use Newton's method for refinement\n");
+    //     int iter;
+    //     for (iter=0; iter<100; iter++) {
+    //         double inv_o = exp(-psi/a);
+    //         double log_o = psi/a;
+    //         double s = (xM-uold) / deltat / 2.0 / v0;
 
-            double F = a * asinh_exp(s, inv_o, log_o) * sigma - tau + eta * (xM-uold) / deltat;
+    //         double F = a * asinh_exp(s, inv_o, log_o) * sigma - tau + eta * (xM-uold) / deltat;
 
-            double dFdx = a  / deltat / 2.0 / v0 * 1.0/sqrt(s*s + inv_o*inv_o) * sigma + eta/deltat;
+    //         double dFdx = a  / deltat / 2.0 / v0 * 1.0/sqrt(s*s + inv_o*inv_o) * sigma + eta/deltat;
             
-            // std::cout << "dF " << dFdx-dFdx_ << std::endl;
-            double dx = F / dFdx;
+    //         // std::cout << "dF " << dFdx-dFdx_ << std::endl;
+    //         double dx = F / dFdx;
 
-            if (abs(dx)/abs(xM) < 1e-20){
-                break;
-            }
-            xM -= dx;
-        }
-        printf("Newton's iteration: %d\n", iter);
-    }
+    //         if (abs(dx) < 1e-10){
+    //             break;
+    //         }
+    //         xM -= dx;
+    //     }
+    //     printf("Newton's iteration: %d\n", iter);
+    // }
     // printf("Number of iterations = %d\n", numIter);
     return xM; 
 }
@@ -87,7 +107,7 @@ double Bisection(double xR, double xL, double psi,
 void forward(double *u, const double *a, const double *uold, const double v0, const double *psi, const double *sigma, const double *tau,
        double eta, double deltat, int n){
     for (int i=0; i<n; i++) {
-        double xL = uold[i];
+        double xL = uold[i] + 1e-16;
         double xR = uold[i] + deltat * tau[i]/eta;
         u[i] = Bisection(xR, xL, psi[i], a[i], uold[i], deltat, eta, v0, tau[i], sigma[i]);
   }
