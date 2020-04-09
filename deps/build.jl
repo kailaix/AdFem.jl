5d434ac68be3130a958e55e4f58745486fff2b5b
Kailai Xu <kailaix@stanford.edu>
2020-04-08 20:00:16 -0700

    accelerate building

--- deps/build.jl
+++ deps/build.jl
@@ -1,32 +1,10 @@
 using ADCME
-function compile(DIR)
-    PWD = pwd()
-    cd(DIR)
-    try
-    rm("build", force=true, recursive=true)
-    catch
-	end
-try
-	mkdir("build")
-    catch
-	end
 
-    cd("build")
-    ADCME.cmake()
-    ADCME.make()
-    cd(PWD)
-end
+rm("build", force=true, recursive=true)
+mkdir("$(@__DIR__)/build")
+cd("build")
+ADCME.cmake()
+ADCME.make()
+cd("..")
 
-
-compile("$(@__DIR__)/DirichletBD")
-compile("$(@__DIR__)/FemStiffness")
-compile("$(@__DIR__)/FemStiffness1")
-compile("$(@__DIR__)/Strain")
-compile("$(@__DIR__)/Strain1")
-compile("$(@__DIR__)/StrainEnergy")
-compile("$(@__DIR__)/StrainEnergy1")
-compile("$(@__DIR__)/SpatialFemStiffness")
-compile("$(@__DIR__)/ComputeVel")
-compile("$(@__DIR__)/VolumetricStrain")
-compile("$(@__DIR__)/SpatialVaryingTangentElastic")
 
