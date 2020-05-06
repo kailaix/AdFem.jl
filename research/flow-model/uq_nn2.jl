88baea99e0c6f1ef790b8b241c4d2740fc0583ad
Kailai Xu <kailaix@stanford.edu>
2020-05-05 22:22:58 -0700

    upload uq_nn2 for testing

--- research/flow-model/uq_nn2.jl
+++ research/flow-model/uq_nn2.jl
@@ -29,41 +29,62 @@ function κfc(x, θ)
     xc = fem_to_fvm(x, m, n, h)
     xc = reshape(repeat(xc, 1, 4), (-1,))
     @assert length(xc)==4*m*n
-    1 + abs(squeeze(fc(reshape(xc, (-1,1)), [20,20,20,1], θ)))
+    0.1 + abs(squeeze(fc(reshape(xc, (-1,1)), [20,20,20,1], θ)))
 end
 
 function param(x, θ)
     xc = fem_to_fvm(x, m, n, h)
     xc = reshape(repeat(xc, 1, 4), (-1,))
-    return 1 + 1/(1+xc^2) + 100xc^2
+    return 0.1 + 1/(1+xc^2) + 100xc^2
 end
 
 function poisson1d_nn(fn)    
+    θ = missing
     function f(θ, u)
         κ = fn(u, θ)
-        κ = compute_space_varying_tangent_elasticity_matrix(κ, m, n, h)
-        K = compute_fem_stiffness_matrix1(κ, m, n, h)
+        K = compute_space_varying_tangent_elasticity_matrix(κ, m, n, h)
+        K = compute_fem_stiffness_matrix1(K, m, n, h)
         K, _ = fem_impose_Dirichlet_boundary_condition1(K, bdnode, m, n, h)
         rhs = K * u 
         rhs = rhs[not_bdnode] - compute_fem_source_term1(ones(4m*n), m, n, h)[not_bdnode]
         rhs, gradients(rhs, u)
     end
-    θ = Variable(ae_init([1, 20, 20, 20, 1]))
+    if fn==κfc
+        θ = Variable(ae_init([1, 20, 20, 20, 1]))
+    end
     u = newton_raphson_with_grad(f, constant(zeros((m+1)*(n+1))) ,θ)
     κnn = fn(u, θ)
     return u, κnn, θ 
 end
 
+function jacobian(u, θ)
+    κ = fn(u, θ)
+    K = compute_space_varying_tangent_elasticity_matrix(κ, m, n, h)
+    K = compute_fem_stiffness_matrix1(K, m, n, h)
+    K, _ = fem_impose_Dirichlet_boundary_condition1(K, bdnode, m, n, h)
+    rhs = K * u 
+    rhs = rhs[not_bdnode] - compute_fem_source_term1(ones(4m*n), m, n, h)[not_bdnode]
+    J = gradients(rhs, u)
+    F = gradients(rhs, κ)
+    -J\F
+end
+
 ADCME.options.newton_raphson.verbose=true
 ADCME.options.newton_raphson.rtol = 1e-4
 ADCME.options.newton_raphson.tol = 1e-4
-y, _, _ = poisson1d_nn(param)
+y, κ, _ = poisson1d_nn(param)
 
 sess = Session(); init(sess)
 SOL = run(sess, y)
-# error()
 
-# inverse modeling 
+
+umax = maximum(SOL)
+xc = LinRange(0, umax, 100)|>Array
+u1 = @. 0.1 + 1/(1+xc^2) + 100xc^2
+close("all")
+plot(xc, u1)
+title("K(u)")
+savefig("nn2_ku_plot.png")
 
 y, κnn, θ  = poisson1d_nn(κfc)
 
@@ -78,7 +99,7 @@ init(sess)
 BFGS!(sess, loss, 200)
 
 
-
+close("all")
 figure(figsize=(10,4))
 subplot(121)
 visualize_scalar_on_fem_points(SOL, m, n, h)
@@ -86,26 +107,37 @@ subplot(122)
 visualize_scalar_on_fem_points(run(sess, y), m, n, h)
 plot(xy_fem[idx,1], xy_fem[idx,2], "o", c="red", label="Observation")
 legend()
+savefig("nn2_sol_compare.png")
 
 
+close("all")
 figure(figsize=(10,4))
 subplot(121)
-visualize_scalar_on_gauss_points(κ, m, n, h)
+visualize_scalar_on_gauss_points(run(sess, κ), m, n, h)
 title("Exact \$K(x, y)\$")
 subplot(122)
 visualize_scalar_on_gauss_points(run(sess, κnn), m, n, h)
 title("Estimated \$K(x, y)\$")
+savefig("nn2_sol_compare.png")
+
+u0 = constant(LinRange(0,umax,100)|>Array )
+z = 1 + abs(squeeze(fc(reshape(u0, (-1,1)), [20,20,20,1], θ)))
+plot(run(sess, u0), run(sess, z), "--")
+plot(xc, u1, "-")
+savefig("nn2_ucurve.png")
 
 
-H = gradients(obs, κnn) 
-H = run(sess, H)
+H = jacobian(y, θ)
+H = run(sess, H)[idx,:]
 y = OBS 
 hs = run(sess, obs)
-R = (1e-1)^2*diagm(0=>ones(length(obs)))
 s = run(sess, κnn)
-Q = (1e-2)^2*diagm(0=>ones(length(κnn)))
+
+R = (1e-2)^2*diagm(0=>ones(length(obs)))
+Q = (1)^2*diagm(0=>ones(length(κnn)))
 μ, Σ = uqnlin(y, hs, H, R, s, Q)
 
+error()
 σ = diag(Σ)
 figure(figsize=(10,4))
 subplot(121)
