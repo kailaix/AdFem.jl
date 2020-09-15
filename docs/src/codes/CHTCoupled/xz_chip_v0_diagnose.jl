3aeccf1fa88cf6791fda6d4960340e361bc62157
Tiffany Fan <tiffan@stanford.edu>
2020-09-14 19:58:06 -0700

    adapt xz_chip_v0 for running on cluster

--- docs/src/codes/CHTCoupled/xz_chip_v0_diagnose.jl
+++ docs/src/codes/CHTCoupled/xz_chip_v0_diagnose.jl
@@ -8,22 +8,35 @@ using ADCMEKit
 
 Random.seed!(118)
 
-# geometry setup in domain [0,1]^2
-solid_left = 0.45
-solid_right = 0.55
-solid_top = 0.5;
-solid_bottom = 0.52
+# trial number specifies a folder to store results
+trialnum = 6
 
-chip_left = 0.48
-chip_right = 0.52
+# geometry setup in domain [0,1]^2
+solid_left = 0.4
+solid_right = 0.6
+solid_top = 0.5
+solid_bottom = 0.6
+
+chip_left = 0.46
+chip_right = 0.54
 chip_top = 0.5
-chip_bottom = 0.505
+chip_bottom = 0.52
+
+# solid_left = 0.45
+# solid_right = 0.55
+# solid_top = 0.5
+# solid_bottom = 0.52
+
+# chip_left = 0.48
+# chip_right = 0.52
+# chip_top = 0.5
+# chip_bottom = 0.505
 
 k_mold = 0.014531
 k_chipdie = 2.60475
 k_air = 0.64357
 nu = 0.47893 # equal to 1/Re
-power_source = 0.06189 #82.46295 = 1.0e6 divide by air rho cp   #0.0619 = 1.0e6 divide by chip die rho cp
+power_source = 82.46295 #82.46295 = 1.0e6 / air rho cp   #0.0619 = 1.0e6 divide by chip die rho cp
 buoyance_coef = 299102.83
 
 u_std = 0.001
@@ -48,10 +61,10 @@ end
 
 
 
-m = 200
-n = 200
+m = 50
+n = 50
 h = 1/n
-NT = 7    # number of iterations for Newton's method, 8 is good for m=400
+NT = 10    # number of iterations for Newton's method, 8 is good for m=400
 
 
 # compute solid indices and chip indices
@@ -93,7 +106,7 @@ end
 xy = fem_nodes(m, n, h)
 chip_x, chip_y = xy[chip_fem_idx, 1], xy[chip_fem_idx, 2]
 # k_chip = @. k_nn(chip_x, chip_y); k_chip=stack(k_chip)
-k_chip = Variable(ones(length(chip_fem_idx)))
+k_chip = Variable(k_mold .* ones(length(chip_fem_idx)))
 k_chip_exact = @. k_exact(chip_x, chip_y)
 
 
@@ -103,13 +116,8 @@ k_fem = scatter_update(k_fem, chip_fem_idx, k_chip)
 kgauss = fem_to_gauss_points(k_fem, m, n, h)
 
 heat_source_fem = zeros((m+1)*(n+1))
-heat_source_fem[chip_fem_idx] .= power_source #/ h^2
-heat_source_fem[chip_fem_top_idx] .= 82.46295
+heat_source_fem[chip_fem_top_idx] .= power_source
 heat_source_gauss = fem_to_gauss_points(heat_source_fem, m, n, h)
-
-# chip_gauss_idx = [ 4 .* chip_fvm_idx; 4 .* chip_fvm_idx .- 1; 4 .* chip_fvm_idx .- 2; 4 .* chip_fvm_idx .- 3]
-# heat_source_gauss = zeros(4*m*n)
-# heat_source_gauss[chip_gauss_idx] .= power_source
 
 B = constant(compute_interaction_matrix(m, n, h))
 
@@ -258,7 +266,7 @@ function plot_velo_pres_temp_cond(k)
     title("difference in x velocity")
     visualize_scalar_on_fem_points(S[1:(m+1)*(n+1)] .* u_std .- S_true[1:(m+1)*(n+1)] .* u_std, m, n, h);gca().invert_yaxis()
     tight_layout()
-    savefig("xzchip_figures5/xzchipv0_nn_velox$k.png")
+    savefig("xzchip_figures$trialnum/pix/xzchipv0_pix_velox$k.png")
 
     figure(figsize=(15,4))
     subplot(131)
@@ -271,7 +279,7 @@ function plot_velo_pres_temp_cond(k)
     title("difference in y velocity")
     visualize_scalar_on_fem_points(S[(m+1)*(n+1)+1: 2*(m+1)*(n+1)]  .* u_std .- S_true[(m+1)*(n+1)+1: 2*(m+1)*(n+1)] .* u_std, m, n, h);gca().invert_yaxis()
     tight_layout()
-    savefig("xzchip_figures5/xzchipv0_nn_veloy$k.png")
+    savefig("xzchip_figures$trialnum/pix/xzchipv0_pix_veloy$k.png")
 
 
     figure(figsize=(15,4))
@@ -285,7 +293,7 @@ function plot_velo_pres_temp_cond(k)
     title("difference in pressure")
     visualize_scalar_on_fvm_points(S[ 2*(m+1)*(n+1)+1:2*(m+1)*(n+1)+m*n] .* p_std .- S_true[ 2*(m+1)*(n+1)+1:2*(m+1)*(n+1)+m*n] .* p_std,  m, n, h);gca().invert_yaxis()
     tight_layout()
-    savefig("xzchip_figures5/xzchipv0_nn_pres$k.png")
+    savefig("xzchip_figures$trialnum/pix/xzchipv0_pix_pres$k.png")
 
     
     figure(figsize=(15,4))
@@ -299,7 +307,7 @@ function plot_velo_pres_temp_cond(k)
     title("difference in temperature")
     visualize_scalar_on_fem_points(S[ 2*(m+1)*(n+1)+m*n+1:end]  .* T_infty .- S_true[2*(m+1)*(n+1)+m*n+1:end] .* T_infty, m, n, h);gca().invert_yaxis()
     tight_layout()
-    savefig("xzchip_figures5/xzchipv0_nn_temp$k.png")
+    savefig("xzchip_figures$trialnum/pix/xzchipv0_pix_temp$k.png")
 
     figure(figsize=(15,4))
     subplot(131)
@@ -327,7 +335,7 @@ function plot_velo_pres_temp_cond(k)
     title("difference in chip conductivity")
 
     tight_layout()
-    savefig("xzchip_figures5/xzchipv0_nn_cond$k.png")
+    savefig("xzchip_figures$trialnum/pix/xzchipv0_pix_cond$k.png")
 
 end
 
@@ -343,7 +351,7 @@ S = set_shape(stack(S), (NT+1, 2*(m+1)*(n+1)+m*n+(m+1)*(n+1)))
 # output = run(sess, S)
 V_computed = S[end, :]
 
-V_data = matread("xzchipv0_fn_data_m$(m)_n$n.mat")["V"]
+V_data = matread("xzchip_figures$trialnum/xzchipv0_fn_data_m$(m)_n$n.mat")["V"]
 
 # sample_size = 100
 # idx = rand(1:(m+1)*(n+1), sample_size)
@@ -368,6 +376,8 @@ sess = Session(); init(sess)
 
 @info run(sess, loss, k_chip=>k_chip_exact)
 lineview(sess, k_chip, loss, k_chip_exact, one.(k_chip_exact))
+savefig("xzchip_figures$trialnum/lineview.png")
+
 # for k = 1:100
 #     loss_ = BFGS!(sess, loss, max_iter)
 #     matwrite("xzchip_figures5/loss$k.mat", Dict("L"=>loss_))
