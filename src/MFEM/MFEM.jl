ef065a8902184bdde32d727c3dc28a710a56a1c5
Kailai Xu <kailaix@hotmail.com>
2020-09-15 15:13:34 -0700

    improve doc

--- src/MFEM/MFEM.jl
+++ src/MFEM/MFEM.jl
@@ -4,6 +4,35 @@ export install_mfem, Mesh, get_ngauss, get_area
 LIBMFEM = joinpath(@__DIR__, "..", "..",  "deps", "MFEM", "build", get_library_name("nnfem_mfem"))
 libmfem = missing 
 
+@doc raw"""
+`Mesh` holds data structures for an unstructured mesh. 
+
+- `nodes`: a $n_v \times 2$ coordinates array
+- `elems`: a $n_e \times 3$ connectivity matrix, 1-based. 
+
+Internally, the mesh `mmesh` is represented by a collection of `NNFEM_Element` object with some other attributes
+```c++
+int nelem; // total number of elements
+int nnode; // total number of nodes
+int ngauss; // total number of Gauss points
+int order; // quadrature integration order
+MatrixXd GaussPts; // coordinates of Gauss quadrature points
+std::vector<NNFEM_Element*> elements; // array of elements
+```
+
+The `NNFEM_Element` has data
+```c++
+VectorXd h;   // basis functions, 3 × ng  
+VectorXd hx;  // x-directional basis functions, 3 × ng  
+VectorXd hy;  // y-directional basis functions, 3 × ng  
+VectorXd w;   // weight vectors, ng  
+double area;  // area of the triangle
+MatrixXd coord; // coordinates array, 3 × 2
+int node[3]; // global index of the vertices
+int nnode; // total number of nodes 
+int ngauss; // total number of Gauss points
+```
+"""
 mutable struct Mesh
     nodes::Array{Float64, 2}
     elems::Array{Int64, 2}
@@ -18,6 +47,12 @@ mutable struct Mesh
     end
 end
 
+@doc raw"""
+    Mesh(m::Int64, n::Int64, h::Float64; order::Int64 = 2)
+
+Constructs a mesh of a rectangular domain. The rectangle is split into $m\times n$ cells, and each cell is further split into two triangles. 
+`order` specifies the quadrature rule order. 
+"""
 function Mesh(m::Int64, n::Int64, h::Float64; order::Int64 = 2)
     coords = zeros((m+1)*(n+1), 2)
     elems = zeros(Int64, 2*m*n, 3)
@@ -40,16 +75,30 @@ function Mesh(m::Int64, n::Int64, h::Float64; order::Int64 = 2)
     Mesh(coords, elems, order)
 end
 
+"""
+    get_ngauss(mesh::Mesh) 
+
+Return the total number of Gauss points. 
+"""
 function get_ngauss(mesh::Mesh)
     return @eval ccall((:mfem_get_ngauss, $LIBMFEM), Cint, ())
 end
 
+"""
+    get_ngauss(mesh::Mesh) 
+
+Return the areas of triangles as an array. 
+"""
 function get_area(mesh::Mesh)
     a = zeros(size(mesh.elems,1))
     @eval ccall((:mfem_get_area, $LIBMFEM), Cvoid, (Ptr{Cdouble}, ), $a)
     a
 end
 
+
+"""
+    gauss_nodes(mesh::Mesh)
+"""
 function gauss_nodes(mesh::Mesh)
     ngauss = get_ngauss(mesh)
     x = zeros(ngauss)
@@ -58,7 +107,9 @@ function gauss_nodes(mesh::Mesh)
     [x y]
 end
 
-
+"""
+    fem_nodes(mesh::Mesh)
+"""
 function fem_nodes(mesh::Mesh)
     mesh.nodes
 end
