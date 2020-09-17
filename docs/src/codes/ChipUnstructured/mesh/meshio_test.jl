9c4760661a9a314b886e08c04106a1cc909b3162
Tiffany Fan <tiffan@stanford.edu>
2020-09-17 14:12:37 -0700

    add stl mesh file

--- docs/src/codes/ChipUnstructured/mesh/meshio_test.jl
+++ docs/src/codes/ChipUnstructured/mesh/meshio_test.jl
@@ -1,14 +1,17 @@
 using PoreFlow
 
 # filename = "/Users/tiffan/Desktop/Research/Physics_Informed_ML/ANSYS/VS_code/PoreFlow.jl/docs/src/codes/ChipUnstructured/mesh/mesh_file_CHT2D.msh"
-filename = "/Users/tiffan/Desktop/Research/Physics_Informed_ML/ANSYS/VS_code/PoreFlow.jl/docs/src/codes/ChipUnstructured/mesh/mesh_file_CHT2D_v01.msh"
-file_format = "ansys"
+# filename = "/Users/tiffan/Desktop/Research/Physics_Informed_ML/ANSYS/VS_code/PoreFlow.jl/docs/src/codes/ChipUnstructured/mesh/mesh_file_CHT2D_v01.msh"
+# file_format = "ansys"
+
+filename = "/Users/tiffan/Desktop/Research/Physics_Informed_ML/ANSYS/VS_code/PoreFlow.jl/docs/src/codes/ChipUnstructured/mesh/CHT_2D.stl"
+ file_format = "stl"
 
 meshio = PoreFlow.get_meshio()
 mesh = meshio.read(filename, file_format = file_format)
 mesh.points[:,1:2]
 mesh.cells[1][2] .+ 1
 
-mesh = Mesh("mesh/mesh_file_CHT2D.msh", file_format="ansys")
-# visualize_mesh(mesh)
-# savefig("mesh.png")
+mesh = Mesh(filename = filename, file_format = file_format)
+visualize_mesh(mesh)
+savefig("mesh.png")
