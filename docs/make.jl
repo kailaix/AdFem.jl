41a81152b4cd36ef4df4c9062e679507b38f00d1
Kailai Xu <kailaix@hotmail.com>
2020-10-12 11:22:57 -0700

    rearrange doc

--- docs/make.jl
+++ docs/make.jl
@@ -4,19 +4,20 @@
 push!(LOAD_PATH, "../src/")
 using Documenter, PoreFlow
 makedocs(sitename="PoreFlow", modules=[PoreFlow],
-pages = Any[
-    "index.md",
-    "gallery.md",
-    "Examples"=>Any["coupled.md", "staticelasticity.md", "plasticity.md", 
-        "viscoelasticity.md", "viscoelasticity_earth.md", "earthquake.md", 
-        "heatequation.md","elastodynamics.md", "twophaseflow.md", "NavierStokes2.md",
-        "SteadyStateNavierStokes.md", "CHTCoupled.md"],
-    "Inverse Modeling"=>Any["inverse.md", "inv_viscoelasticity.md", "coupled_viscoelasticity.md",
+    pages = Any[
+        "index.md",
+        "Tutorial"=>Any["gallery.md"],
+        "Forward Computation"=>Any["coupled.md", "staticelasticity.md", "plasticity.md", 
+            "viscoelasticity.md", "viscoelasticity_earth.md", "earthquake.md", 
+            "heatequation.md","elastodynamics.md", "twophaseflow.md", "NavierStokes2.md",
+            "SteadyStateNavierStokes.md", "CHTCoupled.md"],
+        "Inverse Modeling"=>Any["inverse.md", "inv_viscoelasticity.md", "coupled_viscoelasticity.md",
             "inv_twophaseflow.md", "inv_viscoelasticity_nonparametric.md", "inv_viscoelasticity_earth.md"],
-    "Unstructured Mesh"=>Any["mfem_tutorial.md", "mfem_mesh.md", "dev_unstructured.md", "BDMElement.md"],
-    "api.md"
-],
-authors = "Kailai Xu")
+        "Advanced Topics"=>Any["mfem_tutorial.md", "mfem_mesh.md", "dev_unstructured.md", "BDMElement.md"],
+        "api.md"
+    ],
+    authors = "Kailai Xu"
+)
 
 deploydocs(
     repo = "github.com/kailaix/PoreFlow.jl.git",
