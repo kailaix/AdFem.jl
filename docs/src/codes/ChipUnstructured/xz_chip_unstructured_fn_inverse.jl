1b96d41914fb96fee7caf1569c8d708c1e5dcb0a
Tiffany Fan <tiffan@stanford.edu>
2020-10-15 10:56:24 -0700

    sort files

--- docs/src/codes/ChipUnstructured/xz_chip_unstructured_fn_inverse.jl
+++ docs/src/codes/ChipUnstructured/xz_chip_unstructured_fn_inverse.jl
@@ -5,24 +5,23 @@ using PyPlot
 matplotlib.use("agg") # or try "macosx"
 using SparseArrays
 
-# geometry setup in domain [0,1]^2
-solid_left = 0.45
-solid_right = 0.55
-solid_top = 0.5
-solid_bottom = 0.52
-
-chip_left = 0.48
-chip_right = 0.52
-chip_top = 0.5
-chip_bottom = 0.505
-
-delta=1e-5
-delta2=1e-5
+trialnum = 1
+include("chip_unstructured_geometry.jl")
+include("plot_inverse_one_iter.jl")
 
 k_mold = 0.014531
-# k_chip = 2.60475
-k_chip = Variable(1.0)
+k_chip_ref = 2.60475
 k_air = 0.64357
+
+function k_exact(x, y)
+    k_mold + 1000 * k_chip_ref * (x-0.49)^2 / (1 + x^2)
+end
+
+function k_nn(x, y)
+    out = abs(fc(x, [20,20,20,1])) .+ k_mold
+    squeeze(out)
+end
+
 nu = 0.47893  # equal to 1/Re
 power_source = 82.46295  #82.46295 = 1.0e6 divide by air rho cp   #0.0619 = 1.0e6 divide by chip die rho cp
 buoyance_coef = 299102.83
@@ -31,107 +30,21 @@ u_std = 0.001
 p_std = 0.000001225
 T_infty = 300
 
-filename = "mesh/CHT_2D.stl"
-file_format = "stl"
-mesh = Mesh(filename, file_format = file_format, degree=2)
-mesh = Mesh(mesh.nodes ./ 0.030500000342726707, mesh.elems, -1, 2)
-h = 0.1
+NT = 15    # number of iterations for Newton's method, 8 is good for m=400
 
-# m = 100
-# n = 100
-# h = 1/n
-# mesh = Mesh(m, n, h, degree=2)
-
-nnode = mesh.nnode
-nedge = mesh.nedge
-ndof = mesh.ndof
-nelem = mesh.nelem
-ngauss = get_ngauss(mesh)
-
-NT = 10    # number of iterations for Newton's method, 8 is good for m=400
-
-
-# compute solid indices and chip indices
-solid_fem_idx = Array{Int64, 1}([])
-solid_fvm_idx = Array{Int64, 1}([])
-chip_fem_idx = Array{Int64, 1}([])
-# chip_fvm_idx = Array{Int64, 1}([])
-chip_fem_top_idx = Array{Int64, 1}([])
-bd = Array{Int64, 1}([])
-fvm_bd = Array{Int64, 1}([])
-
-
-
-for j = 1:nnode
-    nodex, nodey = mesh.nodes[j, 1], mesh.nodes[j, 2]
-    if nodex >= solid_left-delta2 && nodex <= solid_right+delta2 && nodey >= solid_top-delta2 && nodey <= solid_bottom+delta2
-        # print(i, j)
-        global solid_fem_idx = [solid_fem_idx; j]
-        if nodex >= chip_left-delta2 && nodex <= chip_right+delta2 && nodey >= chip_top-delta2 && nodey <= chip_bottom+delta2
-            global chip_fem_idx = [chip_fem_idx; j]
-            if nodey <= chip_top+delta2
-                global chip_fem_top_idx = [chip_fem_top_idx; j]
-            end
-        end
-    end
-    if abs(nodex-0.0) <= delta || abs(nodex-1.0) <= delta || abs(nodey-0.0) <= delta || abs(nodey-1.0) <= delta
-        global bd = [bd; j]
-    end
+xy = mesh.nodes 
+xy2 = zeros(mesh.nedge, 2)
+for i = 1:mesh.nedge
+    xy2[i,:] = (mesh.nodes[mesh.edges[i,1], :] + mesh.nodes[mesh.edges[i,2], :])/2
 end
+xy = [xy;xy2]
 
-# fix chip_fem_top_idx
-if size(chip_fem_top_idx, 1) == 0
-    chip_fem_top_idx = chip_fem_idx
-end
-
-for j = 1:nedge
-    edgex, edgey = (mesh.nodes[mesh.edges[j, 1], :] .+ mesh.nodes[mesh.edges[j, 2], :]) ./ 2
-    if edgex >= solid_left-delta2 && edgex <= solid_right+delta2 && edgey >= solid_top-delta2 && edgey <= solid_bottom+delta2
-        # print(i, j)
-        global solid_fem_idx = [solid_fem_idx; nnode + j]
-        if edgex >= chip_left-delta2 && edgex <= chip_right+delta2 && edgey >= chip_top-delta2 && edgey <= chip_bottom+delta2
-            global chip_fem_idx = [chip_fem_idx; nnode + j]
-            if edgex >= chip_left-delta2 && edgex <= chip_right+delta2 && edgey >= chip_top-delta2 && edgey <= chip_top+delta2
-                global chip_fem_top_idx = [chip_fem_top_idx; nnode + j]
-            end
-        end
-    end
-    if abs(edgex-0.0) <= delta || abs(edgex-1.0) <= delta || abs(edgey-0.0) <= delta || abs(edgey-1.0) <= delta
-        global bd = [bd; nnode + j]
-    end
-end
-
-# bd_vec = zeros(ndof)
-# for i in 1:length(bd)
-#     bd_vec[ bd[i] ] = 1.0
-# end
-# figure();visualize_scalar_on_fem_points(bd_vec[1:nnode],mesh,with_mesh=true);savefig("bd.pdf")
-
-# solid_vec = zeros(ndof)
-# for i in 1:length(solid_fem_idx)
-#     solid_vec[ solid_fem_idx[i] ] = 1.0
-# end
-# for i in 1:length(chip_fem_idx)
-#     solid_vec[ chip_fem_idx[i] ] = 2.0
-# end
-# figure();visualize_scalar_on_fem_points(solid_vec[1:nnode],mesh,with_mesh=true);savefig("solid.pdf")
-
-gaussxy = gauss_nodes(mesh)
-
-for i in 1:nelem
-    gaussx, gaussy = mean(gaussxy[6*i-5: 6*i, 1]), mean(gaussxy[6*i-5: 6*i, 2])
-    # print("\nx=",gaussx, " y=",gaussy)
-    if gaussx >= solid_left-delta2 && gaussx <= solid_right+delta2 && gaussy >= solid_top-delta2 && gaussy <= solid_bottom+delta2
-        global solid_fvm_idx = [solid_fvm_idx; i]
-    end
-    if abs(gaussy - 0.0) <= h/2 + delta && ( abs(gaussx - 0.0) <= h + delta || abs(gaussx - 1.0) <= h + delta )
-        global fvm_bd = [fvm_bd; i]
-    end
-end
+x, y = xy[chip_fem_idx, 1], xy[chip_fem_idx, 2]
+k_chip = k_nn(x, y)
 
 k_fem = k_air * constant(ones(ndof))
 k_fem = scatter_update(k_fem, solid_fem_idx, k_mold * ones(length(solid_fem_idx)))
-k_fem = scatter_update(k_fem, chip_fem_idx, k_chip * ones(length(chip_fem_idx)))
+k_fem = scatter_update(k_fem, chip_fem_idx, k_chip)
 kgauss = dof_to_gauss_points(k_fem, mesh)  # try change to include k on edges
 
 heat_source_fem = zeros(ndof)
@@ -292,25 +205,33 @@ _, S = while_loop(condition, body, [i, S_arr])
 S = set_shape(stack(S), (NT+1, nelem+3*ndof))
 
 S_computed = S[end, :]
-S_data = matread("xz_chip_unstructured_data.mat")["V"]
+S_data = matread("fn$trialnum/xz_chip_unstructured_data.mat")["V"]
 
-sample_size = 20
-idx = rand(1:ndof, sample_size)
-idx = [idx; ndof .+ idx; 2*ndof+nelem .+ idx] # observe velocity and temperature
-observed_data = S_data[idx]
+# sample_size = 20
+# idx = rand(1:ndof, sample_size)
+# idx = [idx; ndof .+ idx; 2*ndof+nelem .+ idx] # observe velocity and temperature
+# observed_data = S_data[idx]
 
-noise = false
-noise_level = 0.05
-if noise
-    noise_ratio = (1 - noise_level) .+ 2 * noise_level * rand(Float64, size(observed_data)) # uniform on (1-noise_level, 1+noise_level)
-    observed_data = observed_data .* noise_ratio
-end
+# noise = false
+# noise_level = 0.05
+# if noise
+#     noise_ratio = (1 - noise_level) .+ 2 * noise_level * rand(Float64, size(observed_data)) # uniform on (1-noise_level, 1+noise_level)
+#     observed_data = observed_data .* noise_ratio
+# end
 
-loss = mean((S_computed[idx] .- observed_data)^2)
+loss = mean((S_computed .- S_data)^2)
 loss = loss * 1e10
 # ---------------------------------------------------
 # create a session and run 
-max_iter = 10
+max_iter = 1
 sess = Session(); init(sess)
-loss_ = BFGS!(sess, loss, max_iter)
-figure(); semilogy(loss_); savefig("xzchipv0_loss.png")
+
+for k = 1:10000
+    loss_ = BFGS!(sess, loss, max_iter)
+    printstyled("[#iter $k] loss=$loss_\n", color=:green)
+    matwrite("fn$trialnum/loss$k.mat", Dict("L"=>loss_))
+    close("all"); semilogy(loss_); title("loss vs. iteration")
+    savefig("fn$trialnum/nn_loss$k.png")
+    plot_velo_pres_temp_cond(k)
+    ADCME.save(sess, "fn$trialnum/nn_sess$k.mat")
+end
--- docs/src/codes/ChipUnstructured/xz_chip_unstructured_pix_inverse.jl
+++ docs/src/codes/ChipUnstructured/xz_chip_unstructured_pix_inverse.jl
@@ -11,18 +11,8 @@ k_mold = 0.014531
 k_chip_ref = 2.60475
 k_air = 0.64357
 
-paramA = Variable(2.0)
-paramB = Variable(0.5)
-paramC = Variable(2.0)
-
 function k_exact(x, y)
     k_mold + 1000 * k_chip_ref * (x-0.49)^2 / (1 + x^2)
-end
-
-function k_nn(x, y)
-    # out = abs(fc(x, [20,20,20,1])) .+ k_mold
-    # squeeze(out)
-    k_mold + 1000 * paramA * (x-paramB)^2 ./ (1 + paramC * x.^2)
 end
 
 nu = 0.47893  # equal to 1/Re
@@ -35,24 +25,7 @@ T_infty = 300
 
 NT = 14    # number of iterations for Newton's method, 8 is good for m=400
 
-xy = mesh.nodes 
-xy2 = zeros(mesh.nedge, 2)
-for i = 1:mesh.nedge
-    xy2[i,:] = (mesh.nodes[mesh.edges[i,1], :] + mesh.nodes[mesh.edges[i,2], :])/2
-end
-xy = [xy;xy2]
-
-x, y = xy[chip_fem_idx, 1], xy[chip_fem_idx, 2]
-k_chip = k_nn(x, y)
-# k_chip = eval_f_on_dof_pts(k_nn, mesh)[chip_fem_idx]
-# k_chip=stack(k_chip)
-
-# k_fem = k_air * constant(ones(ndof))
-# k_fem = scatter_update(k_fem, solid_fem_idx, k_mold * ones(length(solid_fem_idx)))
-# k_fem = scatter_update(k_fem, chip_fem_idx, k_chip)
-# kgauss = dof_to_gauss_points(k_fem, mesh)
-# LaplaceK = constant(compute_fem_laplace_matrix1(kgauss, mesh))
-
+k_chip = Variable(ones(length(chip_fem_idx))) .* k_mold
 
 heat_source_fem = zeros(ndof)
 heat_source_fem[chip_fem_top_idx] .= power_source
@@ -65,7 +38,7 @@ Laplace = constant(compute_fem_laplace_matrix1(nu * constant(ones(ngauss)), mesh
 # apply Dirichlet to velocity and temperature; set left bottom two points to zero to fix rank deficient problem for pressure
 bd = [bd; bd .+ ndof; 
       fvm_bd .+ 2*ndof; 
-      bd .+ (2*ndof+nelem)]
+     bd .+ (2*ndof+nelem)]
 
 # add solid region into boundary condition for u, v, p, i.e. exclude solid when solving Navier Stokes
 bd = [bd; solid_fem_idx; solid_fem_idx .+ ndof; solid_fvm_idx .+ 2*ndof]
@@ -77,6 +50,7 @@ S_data = matread("fn$trialnum/xz_chip_unstructured_data.mat")["V"]
 
 loss =  mean((S_computed .- S_data)^2)
 loss = loss * 1e10
+# g = gradients(loss, θ)
 
 # ---------------------------------------------------
 _loss = Float64[]
@@ -86,8 +60,8 @@ cb = (vs, iter, loss)->begin
     printstyled("[#iter $iter] loss=$loss\n", color=:green)
     if mod(iter, 10)==1
         close("all")
-        plot_velo_pres_temp_cond(iter)
-        matwrite("fn$trialnum/nn_loss$iter.mat", Dict("iter"=>iter,"loss"=>_loss, "k_chip"=>vs[1]))
+        pix_plot_velo_pres_temp_cond(iter)
+        matwrite("fn$trialnum/pix_loss$iter.mat", Dict("iter"=>iter,"loss"=>_loss, "k_chip"=>vs[1]))
     end
 end
 
