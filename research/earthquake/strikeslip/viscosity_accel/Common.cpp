210e150cb392832b6c51bd50fe27f479ca970d5b
kailaix <kailaix@hotmail.com>
2020-10-18 23:13:46 -0700

    remove research

--- research/earthquake/strikeslip/viscosity_accel/Common.cpp
+++
@@ -1,5 +0,0 @@
-#include "Common.h"
-
-Eigen::SparseLU<SpMat> solver, solvert;
-std::vector<int> iiK, jjK; 
-int count;
--- research/nonlinear_viscoelasticity_save_nn/run.sh
+++
@@ -1,5 +0,0 @@
-for i in 2 
-do 
-srun julia viscoelasticity_nn.jl $i & 
-done 
-wait
