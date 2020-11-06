f6ffc83a65ec36c77a56e5a84b9a48b306ed982f
Tiffany Fan <tiffan@stanford.edu>
2020-11-05 21:02:14 -0800

    update loss vs distance test

--- docs/src/codes/ChipUnstructured/nn_diagnose/nn_diagnose1.jl
+++ docs/src/codes/ChipUnstructured/nn_diagnose/nn_diagnose1.jl
@@ -53,7 +53,7 @@ bd = [bd; bd .+ ndof;
 bd = [bd; solid_fem_idx; solid_fem_idx .+ ndof; solid_fvm_idx .+ 2*ndof]
 
 S0 = constant(zeros(nelem+3*ndof))
-S_data = matread("diagnose1/data.mat")["V"]
+S_data = matread("diagnose1+/data.mat")["V"]
 
 ntest = 100
 max_iter = 20
@@ -67,9 +67,9 @@ for t in 1:ntest
 # begin of FOR LOOP
 # generate one set of NN weights and biases
 
-    n = 901  #ae_num([20,20,20,1]) => 861?
+    n = 901 
     θ = Variable(randn(n))
-    d = rand(1)
+    d = 4.0 * rand() + 1.0 # for diagnose1+, sample distance between 1 and 5
     θ = θ / norm(θ) * d
     k_chip = k_nn(x, y, θ)
 
@@ -82,11 +82,11 @@ for t in 1:ntest
     # ---------------------------------------------------
     _loss = Float64[]
     cb = (vs, iter, loss)->begin 
-        global _loss
+        _loss
         push!(_loss, loss)
-        printstyled("[#iter $iter] loss=$loss\n", color=:green)
+        printstyled("[#t $t] [#iter $iter] loss=$loss\n", color=:green)
         if mod(iter, 5)==1
-            matwrite(string("diagnose1/test$t","_loss$iter.mat"), Dict("iter"=>iter,"loss"=>_loss, "k_chip"=>vs[1], "d"=>d[1]))
+            matwrite(string("diagnose1+/test$t","_loss$iter.mat"), Dict("iter"=>iter,"loss"=>_loss, "k_chip"=>vs[1], "d"=>d[1]))
         end
     end
 
@@ -94,10 +94,10 @@ for t in 1:ntest
     loss = BFGS!(sess, loss, max_iter; vars = [k_chip], callback = cb)
 
     # record distance d; loss after a fixed number of iterations (converge or not?)
-    dist_test[t] = d[1]
+    dist_test[t] = d
     loss_test[t] = loss[end, 1]
 
-    matwrite("diagnose1/summary$t.mat", Dict("dist"=>dist_test,"loss"=>loss_test))
+    matwrite("diagnose1+/summary$t.mat", Dict("dist"=>dist_test,"loss"=>loss_test))
 # end of FOR LOOP
 end
 
